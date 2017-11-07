e6aadaeb88d9aa1fbf0a184e234a2d4026cefb21
Duje Roje <duje.roje@include.hr>
2017-11-07 14:13:24 +0100

    readme file update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pipeproxy',
-      version='0.5',
+      version='0.6',
       description='A sort-of-proxy solution for multiprocess communication using Pipe from mulitprocessing.',
       url='https://github.com/dRoje/pipe-proxy.git',
       author='Duje Roje',
@@ -12,6 +12,14 @@ setup(name='pipeproxy',
                 'pipeproxy.lib.objectProxy',
                 'pipeproxy.lib.objectProxy.proxyMessanger',
                 'pipeproxy.lib.proxyListener',
-                'pipeproxy.lib.proxyMessages'],
+                'pipeproxy.lib.proxyMessages',
+
+                'test_pipeproxy',
+                'test_pipeproxy.test_lib',
+                'test_pipeproxy.test_lib.test_objectProxy',
+                'test_pipeproxy.test_lib.test_objectProxy.test_proxyMessanger',
+                'test_pipeproxy.test_lib.test_proxyListener',
+                'test_pipeproxy.test_lib.test_proxyMessages'],
+
       # package_dir={'pipeproxy': 'lib'},
       zip_safe=False)
